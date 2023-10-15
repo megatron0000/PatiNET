fe93bcc65e810af674f60418146a1f7a2303c009
megatron0000 <vitor_pimenta97@hotmail.com>
2023-10-15 13:06:31 -0400

    chore: add electron forge github publisher

--- forge.config.ts
+++ forge.config.ts
@@ -39,6 +39,18 @@ const config: ForgeConfig = {
       },
     }),
   ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "me",
+          name: "PatiNET",
+        },
+        prerelease: true,
+      },
+    },
+  ],
 };
 
 export default config;
