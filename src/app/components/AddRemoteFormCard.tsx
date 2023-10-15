b838e454d1c0a16f4c393d96939c752ff3692e72
megatron0000 <vitor_pimenta97@hotmail.com>
2023-10-15 11:08:34 -0400

    feat: add remote when hit Enter

--- src/app/components/AddRemoteFormCard.tsx
+++ src/app/components/AddRemoteFormCard.tsx
@@ -46,7 +46,9 @@ export function AddRemoteFormCard({
   const [host, setHost] = useState("");
   const [port, setPort] = useState("");
 
-  async function handleSubmit() {
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
     const localPortError = askLocalPort
       ? validatePort(localPort, { acceptEmpty: true, allowPrivileged: false })
       : null;
@@ -91,78 +93,80 @@ export function AddRemoteFormCard({
       style={style}
     >
       <CardContent>
-        <Box
-          marginBottom={2}
-          display={"flex"}
-          flexDirection={"row"}
-          flexWrap={"wrap"}
-          alignItems={"stretch"}
-          justifyContent={"space-between"}
-        >
-          <Box fontWeight="bold" display={"flex"} alignItems={"flex-start"}>
-            {askLocalPort ? (
+        <form onSubmit={handleSubmit}>
+          <Box
+            marginBottom={2}
+            display={"flex"}
+            flexDirection={"row"}
+            flexWrap={"wrap"}
+            alignItems={"stretch"}
+            justifyContent={"space-between"}
+          >
+            <Box fontWeight="bold" display={"flex"} alignItems={"flex-start"}>
+              {askLocalPort ? (
+                <TextField
+                  label="Porta local (opcional)"
+                  placeholder="e.g. 8080"
+                  value={localPort}
+                  onChange={(e) => setLocalPort(e.target.value.trim())}
+                  error={localPortError !== null}
+                  helperText={localPortError}
+                  margin="none"
+                  size="small"
+                  sx={{ mb: 1, width: 190 }}
+                />
+              ) : (
+                <Box sx={{ mt: 1 }}>127.0.0.1:{propsLocalPort || "?"}</Box>
+              )}
+              <ComputerIcon sx={{ mx: 2, mt: 1 }} />
+            </Box>
+
+            <Box fontWeight="bold" display={"flex"} alignItems={"flex-start"}>
+              <ComputerIcon sx={{ mx: 2, mt: 1 }} />
               <TextField
-                label="Porta local (opcional)"
-                placeholder="e.g. 8080"
-                value={localPort}
-                onChange={(e) => setLocalPort(e.target.value.trim())}
-                error={localPortError !== null}
-                helperText={localPortError}
+                label="Host remoto"
+                placeholder="e.g. 172.24.23.100"
+                value={host}
+                onChange={(e) => setHost(e.target.value.trim())}
+                error={hostError !== null}
+                helperText={hostError}
                 margin="none"
                 size="small"
-                sx={{ mb: 1, width: 190 }}
+                sx={{ mr: 1, mb: 1, width: 200 }}
               />
-            ) : (
-              <Box sx={{ mt: 1 }}>127.0.0.1:{propsLocalPort || "?"}</Box>
-            )}
-            <ComputerIcon sx={{ mx: 2, mt: 1 }} />
+              <TextField
+                label="Porta remota"
+                placeholder="e.g. 8080"
+                value={port}
+                onChange={(e) => setPort(e.target.value.trim())}
+                error={portError !== null}
+                helperText={portError}
+                margin="none"
+                size="small"
+                sx={{ mb: 1, width: 140 }}
+              />
+            </Box>
           </Box>
 
-          <Box fontWeight="bold" display={"flex"} alignItems={"flex-start"}>
-            <ComputerIcon sx={{ mx: 2, mt: 1 }} />
-            <TextField
-              label="Host remoto"
-              placeholder="e.g. 172.24.23.100"
-              value={host}
-              onChange={(e) => setHost(e.target.value.trim())}
-              error={hostError !== null}
-              helperText={hostError}
-              margin="none"
-              size="small"
-              sx={{ mr: 1, mb: 1, width: 200 }}
-            />
-            <TextField
-              label="Porta remota"
-              placeholder="e.g. 8080"
-              value={port}
-              onChange={(e) => setPort(e.target.value.trim())}
-              error={portError !== null}
-              helperText={portError}
-              margin="none"
-              size="small"
-              sx={{ mb: 1, width: 140 }}
-            />
+          <Box display={"flex"} justifyContent={"center"}>
+            <Button
+              type="submit"
+              variant="outlined"
+              color="primary"
+              sx={{ m: 2 }}
+            >
+              OK
+            </Button>
+            <Button
+              onClick={onCancel}
+              variant="outlined"
+              color="secondary"
+              sx={{ m: 2 }}
+            >
+              Cancelar
+            </Button>
           </Box>
-        </Box>
-
-        <Box display={"flex"} justifyContent={"center"}>
-          <Button
-            onClick={handleSubmit}
-            variant="outlined"
-            color="primary"
-            sx={{ m: 2 }}
-          >
-            OK
-          </Button>
-          <Button
-            onClick={onCancel}
-            variant="outlined"
-            color="secondary"
-            sx={{ m: 2 }}
-          >
-            Cancelar
-          </Button>
-        </Box>
+        </form>
       </CardContent>
     </Card>
   );
